afb27f7525f68978639ac13d573335fca90e6b10
Elania <83014218+Elania-Marvers@users.noreply.github.com>
2024-02-21 15:04:18 +0100

    Update captcha.ex

--- lib/captcha.ex
+++ lib/captcha.ex
@@ -1,16 +1,18 @@
 defmodule Captcha do
   # allow customize receive timeout, default: 10_000
-  def get(timeout \\ 1_000) do
+  def get(timeout \\ 10_000) do
     Port.open({:spawn, Path.join(:code.priv_dir(:captcha), "captcha")}, [:binary])
     IO.inspect("GENERATING CAPTCHA")
     # Allow set receive timeout
     receive do
       {_, {:data, data}} ->
+        IO.inspect("SUCESS")
         <<text::bytes-size(5), img::binary>> = data
         {:ok, text, img }
 
       other -> other
     after timeout ->
+      IO.inspect("TIMEOUT")
       {:timeout}
     end
   end
